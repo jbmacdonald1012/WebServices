be679e7ca03ea8d6ef92aec04cc7407397000ff0
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2024-02-17 19:53:30 -0700

    Updates routes to work on Render

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
-router.get('/contacts', contactsController.getAllContacts);
+router.get('/', contactsController.getAllContacts);
 
 router.get('/contacts/:id', contactsController.getSingleContact);
 
-router.post('/contacts', contactsController.createContact);
+router.post('/', contactsController.createContact);
 
 router.put('/contacts/:id', contactsController.updateContact);
 
